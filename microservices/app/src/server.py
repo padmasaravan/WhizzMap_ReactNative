87b887be264b54daa7bd27531adfe4ef92dc07f8
Vinitha-shree <vinishree.97@gmail.com>
2018-02-05 18:52:38 +0530

    files added

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -49,7 +49,7 @@ def directionRoute():
                 #dest_geocode = geocode2.json()
                 #src_latlng = src1_geocode["features"]
                 #dest_latlng= dest_geocode['features'][0]['geometry']['coordinates']
-                print(src1_geocode)
+                print(src1_geocode["type"])
                 return "hii"
                 '''origin = {'type': 'Feature','properties': {'name': 'dummy'},'geometry': {'type': 'Point','coordinates': [0,0]}}
                 origin['properties']['name']=src
