4787cc6dbe07eef7f9c5e2c04ab7d3afdc45aabb
Vinitha-shree <vinishree.97@gmail.com>
2018-02-05 18:33:25 +0530

    files added

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -44,13 +44,13 @@ def directionRoute():
                 #geocoder.session.params['access_token'] == 'sk.eyJ1IjoidmluaXRoYS1zaHJlZSIsImEiOiJjamNjMjkzZ3MwbTc0MndvMndtM2Ewb3lxIn0.cm3yhsou3E8UD0pm1GPKlA'
                 geocode1 = geocoder.forward(src)
                 src_geocode = geocode1.json()
-                src1_geocode = json.dumps(src_geocode)
+                src1_geocode = json.loads(src_geocode)
                 #geocode2 = geocoder.forward(dest)
                 #dest_geocode = geocode2.json()
                 #src_latlng = src1_geocode["features"]
                 #dest_latlng= dest_geocode['features'][0]['geometry']['coordinates']
-                print(src1_geocode["features"][0]["geometry"]["coordinates"])
-                return "hii"
+                #print(src1_geocode["features"][0]["geometry"]["coordinates"])
+                return src1_geocode
                 '''origin = {'type': 'Feature','properties': {'name': 'dummy'},'geometry': {'type': 'Point','coordinates': [0,0]}}
                 origin['properties']['name']=src
                 origin['geometry']['coordinates'] = src_latlng
